da97cbadd742c12182a3bcf64750c8267d7fc026
josephwjcho93@gmail.com <josephcho@Josephs-MacBook-Pro.local>
2024-06-28 14:23:19 -0400

    added server.js codes and jwt

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const usersRouter = require('./controllers/users');
 
 app.use(cors());
 
