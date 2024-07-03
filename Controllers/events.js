756fe8188853c286f949b2829845119158bce020
Clint Eastman <clint@Clints-MacBook-Air.local>
2024-07-03 09:38:13 -0400

    updated packages andupdated files

--- Controllers/events.js
+++ Controllers/events.js
@@ -1,65 +1,60 @@
-const express = require('express');
-const verifyToken = require('../middleware/verify-token.js');
-const Event = require('../models/event.js');
+const express = require("express");
+const verifyToken = require("../middleware/verify-token.js");
+const Event = require("../models/event.js");
 const router = express.Router();
-
-// ========== Public Routes ===========
-
-// ========= Protected Routes =========
 
 router.use(verifyToken);
 
-router.post('/', async (req, res) => {
-    try {
-      req.body.author = req.user._id;
-      const event = await Event.create(req.body);
-      res.status(201).json(event);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error);
-    }
+router.post("/", async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const event = await Event.create(req.body);
+    res.status(201).json(event);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
-router.get('/', async (req, res) => {
-    try {
-      const event = await Event.find({})
-        .populate('author')
-        .sort({ createdAt: 'desc' });
-      res.status(200).json(event);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const event = await Event.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:eventId', async (req, res) => {
-    try {
-      const event = await Event.findById(req.params.eventId).populate([
-        'betters',
-      ]);
-      res.status(200).json(event);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+router.get("/:eventId", async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.eventId).populate([
+      "betters",
+    ]);
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
+router.post("/:eventId/bet", async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const event = await Event.findById(req.params.eventId);
+    event.betters.push(req.body);
+    await event.save();
 
-router.post('/:eventId/bet', async (req, res) => {
-    try {
-      req.body.author = req.user._id;
-      const event = await Event.findById(req.params.eventId);
-      event.betters.push(req.body);
-      await event.save();
-  
-      // Find the newly created comment:
-      const newBet = event.betters[event.betters.length - 1];
-  
-      newBet._doc.better = req.user;
-  
-      // Respond with the newComment:
-      res.status(201).json(newBet);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+    // Find the newly created comment:
+    const newBet = event.betters[event.betters.length - 1];
+
+    newBet._doc.better = req.user;
+
+    // Respond with the newComment:
+    res.status(201).json(newBet);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const usersRouter = require('./controllers/users');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const usersRouter = require("./Controllers/users");
 
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(cors()); 
+app.use(cors());
 // for middleware-Steven
 app.use(express.json());
 
-
-
-app.use('/users', usersRouter);
-
+app.use("/users", usersRouter);
 
 app.listen(3000, () => {
-    console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
+
